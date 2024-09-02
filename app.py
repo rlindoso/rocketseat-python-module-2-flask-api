7ab56fb92c243b6245a2a096f8502e798cd909fe
Rafael Lindoso <rafael.lindoso@gmail.com>
2024-09-01 21:54:49 -0300

    Task model

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request
+from models.Task import Task
 
 app = Flask(__name__)
+
+tasks = []
 
 @app.route("/")
 def hello_world():
     return "Hello world!"
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    data = request.get_json()
+    print(data)
+    return 'Test'
 
 if __name__ == "__main__":
     app.run(debug=True)
